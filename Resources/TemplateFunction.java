d83a0bed6660719aff54926fcff5fb56b5fa7d33
lbobelin <lbobelin@mintcar.imag.fr>
2012-07-12 16:07:14 +0200

    Added templates for Java in Resources so as you can move it to the correct folder.

---
+++ Resources/TemplateFunction.java
@@ -0,0 +1,24 @@
+/*
+ * Generated function Class.
+ */
+package <PACKAGE_NAME>;
+import org.simgrid.msg.Host;
+import org.simgrid.msg.Msg;
+import org.simgrid.msg.MsgException;
+import org.simgrid.msg.Task;
+import org.simgrid.msg.Process;
+
+public class <FUNCTION_NAME> extends Process {
+   public <FUNCTION_NAME>(Host host, String name, String[]args) {
+		super(host,name,args);
+   } 
+    
+   public void main(String[] args) throws MsgException {
+        
+      Msg.info("Starting the function");
+
+	// INSERT YOUR FUNCTION CODE HERE            
+
+      Msg.info("goodbye!");
+    }
+}
